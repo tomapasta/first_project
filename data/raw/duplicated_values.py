7d39a94d57da0efaf4a581d8245dbf6e539bbea7
DavyN25 <davygoupil6@gmail.com>
2025-12-08 17:57:30 +0100

    Version1 of cleanning data upload

---
+++ data/raw/duplicated_values.py
@@ -0,0 +1,18 @@
+# Add all the imports needed by the functions in the project here
+#================================================================
+import pandas as pd
+
+#================================================================
+
+# Remember to modify your functions to use the template shown below
+
+
+	"""
+	
+	This function delete the diplicated rows
+    
+	"""
+
+def delete_duplicate_valuesdf.duplicated()   # shows the duplicated values
+    df.function_drop.duplicates ()   # deleate the duplicated raws
+
---
+++ notebooks/outliers.py
@@ -0,0 +1,20 @@
+# Add all the imports needed by the functions in the project here
+#================================================================
+import pandas as pd
+import seaborn as sns
+#================================================================
+
+# Remember to modify your functions to use the template shown below
+
+
+	"""
+	
+	This is a visualization chart that shows the spread of values and highlights outliers.
+    
+	"""
+	
+	# Your function code here
+
+def function_outliers_values (sns.boxplot(x=davy_df["column"])
+	
+	return boxplot
