c74be2841df318694e49d1c8f6987290e23dc053
Ako <ako.kitamura2211@gmail.com>
2025-12-09 08:30:46 +0100

    functions in one file

--- notebooks/datetime.py
+++
@@ -1,9 +0,0 @@
-#cleaning datetime 
-"""this function convert Release Year columns from data type 'object' to date-time"""
-
-def datetime (): 
- df["release_year"] = pd.to_datetime(df["release_year"],errors="coerce")
- return df 
-#df["Release Year"] = pd.to_numeric(df["Release Year"], errors='coerce')
-#df["Release Year"] = df["Release Year"].astype("Int64")
-datetime () 
